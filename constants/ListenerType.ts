d52398df6430b8515c777ee2ebdb7d7f891f2d35
Nguyen Gia Tuan <garfdev@Nguyens-MacBook-Pro-2.local>
2021-04-14 22:21:35 +0700

    fix(constants): Remove unneedcesary logics

--- constants/ListenerType.ts
+++ constants/ListenerType.ts
@@ -1,14 +1,12 @@
 enum ListenerType {
   GENERAL = 'GENERAL',
-  DEVELOPER = 'DEVELOPER',
-  MANAGE = 'MANAGE'
+  DEVELOPER = 'DEVELOPER'
 }
 
 /////////////////////////////
 
 export const ListenerTypeLabel = {
   [ListenerType.DEVELOPER]: 'Developer only access Commands',
-  [ListenerType.MANAGE]: 'Bot Management Commands',
   [ListenerType.GENERAL]: 'General Commands'
 };
 
